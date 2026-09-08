fdd6b8655171fdb50904b8a30fa304083f337f1c
rjangid631 <rohanjangid631@gmail.com>
2026-09-08 11:39:23 +0530

    updated admin.py for the models

--- users/admin.py
+++ users/admin.py
@@ -1,14 +1,106 @@
 from django.contrib import admin
-from .models import User
-from .models import LegacyPermission, VisitType, UserLocationHistory
+from .models import (
+    User,
+    LegacyPermission,
+    VisitType,
+    UserLocationHistory,
+)
 
-def get_list_display(model):
-    return [field.name for field in model._meta.fields]
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = get_list_display(User)
+    list_display = (
+        "id",
+        "user_name",
+        "full_name",
+        "email",
+        "mobile_number",
+        "user_type",
+        "is_active",
+        "added_date",
+        "updated_date",
+    )
 
-admin.site.register(LegacyPermission)
-admin.site.register(VisitType)
-admin.site.register(UserLocationHistory)
+    list_filter = (
+        "user_type",
+        "is_active",
+        "gender",
+    )
+
+    search_fields = (
+        "user_name",
+        "full_name",
+        "email",
+        "mobile_number",
+    )
+
+    ordering = ("-id",)
+
+
+@admin.register(UserLocationHistory)
+class UserLocationHistoryAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "legacy_location_id",
+        "longitude",
+        "latitude",
+        "timestamp",
+        "slot_booking",
+    )
+
+    list_filter = (
+        "timestamp",
+    )
+
+    search_fields = (
+        "user__user_name",
+        "user__full_name",
+        "legacy_location_id",
+    )
+
+    ordering = ("-timestamp",)
+
+
+@admin.register(LegacyPermission)
+class LegacyPermissionAdmin(admin.ModelAdmin):
+    list_display = (
+        "permission_id",
+        "user",
+        "controller",
+        "action",
+    )
+
+    list_filter = (
+        "controller",
+        "action",
+    )
+
+    search_fields = (
+        "controller",
+        "action",
+        "user__user_name",
+        "user__full_name",
+    )
+
+    ordering = ("permission_id",)
+
+
+@admin.register(VisitType)
+class VisitTypeAdmin(admin.ModelAdmin):
+    list_display = (
+        "visit_type_id",
+        "visit_type_name",
+        "is_active",
+        "trial353",
+    )
+
+    list_filter = (
+        "is_active",
+    )
+
+    search_fields = (
+        "visit_type_name",
+    )
+
+    ordering = ("visit_type_id",)
